d83d3aeac0a0fcbac3510dee84ec717e67d2a294
Auadrian <auadrian@hawaii.edu>
2021-03-28 22:43:05 -1000

    testing Iroh

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import logo from './lock-128.png';
 import './App.css';
 import FirstComponent from './FirstComponent'
 import SecondComponent from './SecondComponent'
+import Iroh from 'iroh';
 
 class App extends Component {
   render() {
     return (
+
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
@@ -24,5 +26,19 @@ class App extends Component {
     );
   }
 }
+
+// function add(a, b) {
+//   return a + b;
+// };
+//
+// let stage = new Iroh.Stage(add.toString());
+// stage.addListener(Iroh.FUNCTION).on("return", function(e) {
+//   if (e.name === "add") e.return = 42;
+// });
+//
+// eval(stage.script);
+//
+// document.body.innerHTML = add(2, 2);
+
 
 export default App;
